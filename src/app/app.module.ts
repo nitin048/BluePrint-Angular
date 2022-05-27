0b94d0cb53414d6ccc80a95bf186eeb24cfd1014
nitin pradhan <nitinpradhan48@gmail.com>
2022-05-27 14:16:03 +0530

    Commit 2
    Added components
    Navbar
    dashboard
    menu
    container
    footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './Dashboard/menu/menu.component';
+import { ContainerComponent } from './Dashboard/container/container.component';
+import { FooterComponent } from './Dashboard/footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MenuComponent,
+    ContainerComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
