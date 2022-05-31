052b2c80637fa36dc76bd6946609d0d8926ed636
kazumakanagi <jensen.adam13@google.com>
2022-05-31 16:53:24 +0530

    added carousel in dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,15 +9,19 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './Dashboard/menu/menu.component';
 import { ContainerComponent } from './Dashboard/container/container.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbdCarouselConfig } from './Dashboard/carousel-config/carousel-config.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; //---------  Important
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    ContainerComponent
+    ContainerComponent,
+    NgbdCarouselConfig
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     NgbModule,BrowserModule, FormsModule, HttpClientModule, NgbModule
   ],
