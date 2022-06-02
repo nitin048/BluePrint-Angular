cdfa0b9fd3881c0939837a35f647af7953841a61
nitin pradhan <nitinpradhan48@gmail.com>
2022-06-02 14:43:59 +0530

    commit-1 2nd june 2022
    integrated text to speech recognition
    with aayang.js lib

--- src/app/Dashboard/menu/menu.component.ts
+++ src/app/Dashboard/menu/menu.component.ts
@@ -1,8 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,NgZone } from '@angular/core';
 import {HttpClient, HttpParams} from '@angular/common/http';
 import {Observable, of, OperatorFunction} from 'rxjs';
 import {catchError, debounceTime, distinctUntilChanged, map, tap, switchMap} from 'rxjs/operators';
 import { Injectable } from '@angular/core';
+
+// ----const variable for text to speech lib----//
+
+declare const annyang: any;
+
+// ----const variable for text to speech lib----//
+
+// --------------- valiable initilization for search bar-----------------//
 
 const WIKI_URL = 'https://en.wikipedia.org/w/api.php';
 const PARAMS = new HttpParams({
@@ -11,6 +19,7 @@ const PARAMS = new HttpParams({
     format: 'json',
     origin: '*'
   }
+// --------------- valiable initilization for search bar-----------------//
 });
 
 @Injectable()
@@ -39,12 +48,95 @@ export class WikipediaService {
 
 
 export class MenuComponent implements OnInit {
+
+
+constructor(private _service: WikipediaService , private ngZone: NgZone)  {}
+
+
+// -----------------------method for text to speech -----------------------------------//
+  voiceActiveSectionDisabled: boolean = true;
+	voiceActiveSectionError: boolean = false;
+	voiceActiveSectionSuccess: boolean = false;
+	voiceActiveSectionListening: boolean = false;
+	voiceText: any;
+
+ 
+
+  initializeVoiceRecognitionCallback(): void {
+		annyang.addCallback('error', (err : any) => {
+      if(err.error === 'network'){
+        this.voiceText = "Internet is require";
+        annyang.abort();
+        this.ngZone.run(() => this.voiceActiveSectionSuccess = true);
+      } else if (this.voiceText === undefined) {
+				this.ngZone.run(() => this.voiceActiveSectionError = true);
+				annyang.abort();
+			}
+		});
+
+		annyang.addCallback('soundstart', (res: any) => {
+      this.ngZone.run(() => this.voiceActiveSectionListening = true);
+		});
+
+		annyang.addCallback('end', () => {
+      if (this.voiceText === undefined) {
+        this.ngZone.run(() => this.voiceActiveSectionError = true);
+				annyang.abort();
+			}
+		});
+
+		annyang.addCallback('result', (userSaid :any) => {
+			this.ngZone.run(() => this.voiceActiveSectionError = false);
+
+			let queryText: any = userSaid[0];
+
+			annyang.abort();
+
+      this.voiceText = queryText;
+
+			this.ngZone.run(() => this.voiceActiveSectionListening = false);
+      this.ngZone.run(() => this.voiceActiveSectionSuccess = true);
+		});
+	}
+
+	startVoiceRecognition(): void {
+    this.voiceActiveSectionDisabled = false;
+		this.voiceActiveSectionError = false;
+		this.voiceActiveSectionSuccess = false;
+    this.voiceText = undefined;
+
+		if (annyang) {
+			let commands = {
+				'demo-annyang': () => { }
+			};
+
+			annyang.addCommands(commands);
+
+      this.initializeVoiceRecognitionCallback();
+
+			annyang.start({ autoRestart: false });
+		}
+	}
+
+	closeVoiceRecognition(): void {
+    this.voiceActiveSectionDisabled = true;
+		this.voiceActiveSectionError = false;
+		this.voiceActiveSectionSuccess = false;
+		this.voiceActiveSectionListening = false;
+		this.voiceText = undefined;
+
+		if(annyang){
+      annyang.abort();
+    }
+	}
+
+
+// -----------------------method for text to speech -----------------------------------//
+
 //----------------------------------method for search bar-------------------------------// 
   model: any;
   searching = false;
   searchFailed = false;
-
-  constructor(private _service: WikipediaService) {}
 
   search: OperatorFunction<string, readonly string[]> = (text$: Observable<string>) =>
     text$.pipe(
@@ -63,8 +155,7 @@ export class MenuComponent implements OnInit {
     )
 
 //----------------------------------method for search bar -------------------------------// 
-
-  Constructor() { }
+ 
   ngOnInit(): void {
   }
 
