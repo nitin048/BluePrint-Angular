cde82c36f298793a4145148dc56e59a728d2c881
pankajsakhare19 <pankajsakhare91@gmail.com>
2022-06-03 15:33:19 +0530

    added app-routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './Components/Header/header.component';
 import { ContainerComponent } from './Components/container/container.component';
@@ -13,6 +11,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NgbdCarouselConfig } from './Components/carousel-config/carousel-config.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FooterComponent } from './Components/footer/footer.component'; //---------  Important
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
